e58ce49e306d5c136779c558c792a3052cc8bdc3
Tiago Silva <tiagoluizrs@gmail.com>
2022-07-02 23:14:14 -0300

    django commit

--- admin/settings/development.py
+++ admin/settings/development.py
@@ -2,7 +2,7 @@ from .settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Crie a secret key para seu ambiente de desenvolvimento
 SECRET_KEY = 'q_a$9$rq9an++-iiy7gn^f=j-n#(zswduraa88jb&2%jizcjvt'
