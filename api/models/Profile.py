66193026ab30a06a095a1c5cfa4ca409501f813b
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-08-25 13:11:01 -0300

    Profile with photo upload

--- api/models/Profile.py
+++ api/models/Profile.py
@@ -19,7 +19,7 @@ class Team(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    image = models.ImageField(upload_to='uploads/profiles/', null=True, blank=True)
+    image = models.TextField(null=True, blank=True)
     team = models.ForeignKey(Team, related_name='team_user', on_delete=models.CASCADE, null=True, blank=True)
     patent = models.ForeignKey(Patent, related_name='patent_user', on_delete=models.CASCADE, null=True, blank=True)
     code_confirm = models.CharField(max_length=6, null=True, blank=True)
